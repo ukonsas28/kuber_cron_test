1222a97c0b49e08281ace85d1d11054b9b7ef377
Alexey Shirokov <shirokovam@lad24.ru>
2023-04-09 13:09:34 +0300

    cron job kuber

---
+++ start.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Запуск миникуба и ожидание старта кластера
+minikube start --vm-driver=virtualbox & start_minikube_id=$!
+
+wait $start_minikube_id
+
+# Устанвливаем ипользование doker из миникуба что бы образы собирались в миникубе
+eval $(minikube -p minikube docker-env)
+
+sleep 3
+
+# Собираем image для дальнейшего запуска контейнера
+docker build -t nodemailer:cur . & create_image_id=$!
+
+wait $create_image_id
+
+#Если уже есть job на запуск пода удаляем ее
+kubectl delete -f cronmailer.yml
+
+#Запускаем job на запуск пода
+kubectl create -f cronmailer.yml
+
+
+#Открываем дашборд для работы 
+minikube dashboard
